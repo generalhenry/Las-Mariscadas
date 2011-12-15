dd7dd872ab78dbbb707c1286333d8d9845c2799f
Henry Allen-Tilford <htilford@gmail.com>
2011-12-15 23:37:03 +0000

    blitz

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 var express = require('express');
 var app = express.createServer();
 app.use(express['static'](__dirname + '/public'));
+
+app.get('/mu-3f11dd3c-7a3285d7-0d67644e-e1742723', function( req, res ) {
+  res.send('42');
+});
 app.listen(process.env.PORT);
